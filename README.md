9ac51bfb36fff79569f4079a6c8a035cf6c1c0c8
danil.gizdatullin <gizdatullindanil@gmail.com>
2016-02-26 02:34:07 +0300

    changed readme

--- README.md
+++ README.md
@@ -10,7 +10,18 @@ current user. The format of timestamp is doesn't matter. If event didn't happen 
 *label;work;separation;marriage*  
 *1;;215;305*  
   
-You can find some examples of using in *./examples/export_data_from_csv.py*  
-  
+## Create a Special Data Format From CSV  
+To build Rules Tree and using Classifier you need to convert your data in special format.  
+**Example**  
+*data = [[['1'], ['2'], ['3']], ..., [['1', '2'], ['3'], ['4']]]*  
+*labels = [1, ..., 0]*  
+You can find some examples of using in *./examples/export_data_from_csv.py*    
   
 ## Building Rules Trie.  
+You can build a simple Rules Trie and Closured Rules Trie, which has only closured patterns.  
+You can find some examples in *./examples/build_rules_trie_on_some_data.py*  
+  
+## Classifier.  
+You can use Simple Classifier, Closure Classifier or Hypothesis Classifier. With this tool you can solve binary and  
+multiclassification tasks.  
+You can find some examples in *./examples/using_classification.py*  
--- examples/export_data_from_csv.py
+++ examples/export_data_from_csv.py
@@ -1,15 +1,20 @@
-from usa.reader import read_from_csv
+from usa.reader import ReadFromCSV
 
-sequence_reader = read_from_csv.SequencesFromFile(file_name='./data/full_data_shuffle.csv',
-                                                  coding_dict={'work': 1,
-                                                               'separation': 2,
-                                                               'partner': 3,
-                                                               'marriage': 4,
-                                                               'children': 5,
-                                                               'parting': 6,
-                                                               'divorce': 7,
-                                                               'education': 8}
-                                                  )
+# create a object of class ReadFromCSV
+# file_name is a path to file with data
+# coding dict is a dict of column names to int numbers
+sequence_reader = ReadFromCSV(file_name='./data/full_data_shuffle.csv',
+                              coding_dict={'work': 1,
+                                           'separation': 2,
+                                           'partner': 3,
+                                           'marriage': 4,
+                                           'children': 5,
+                                           'parting': 6,
+                                           'divorce': 7,
+                                           'education': 8})
+
+# call from_file_to_data_list method to create two lists
+# first list with data, second list with labels
 data, label = sequence_reader.from_file_to_data_list(label_name='label')
 
 print(data)
